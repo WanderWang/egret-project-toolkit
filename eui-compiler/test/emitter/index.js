a2eeb66f6697225705e583b30ac979f669af6b87
unknown <185713114@qq.com>
2020-06-05 13:53:47 +0800

    改善测试用例代码

--- eui-compiler/test/emitter/index.js
+++ eui-compiler/test/emitter/index.js
@@ -2,18 +2,13 @@
 const assert = require('assert');
 const fs = require('fs');
 const path = require('path');
-
+const { describe, it, afterEach } = require('mocha');
 
 const parser = require('../../lib/util/parser')
-const JavaScriptEmitter = require('../../lib/util/emitter2').JavaScriptEmitter;
+const JavaScriptEmitter = require('../../lib/emitter').JavaScriptEmitter;
 const typings = require('../../lib/util/typings');
 
 const esprima = require('esprima');
-
-
-const todos = []
-
-// const dirs = ['scroller'];
 
 
 describe('emitter', () => {
@@ -23,33 +18,23 @@ describe('emitter', () => {
     const dirs = fs.readdirSync(baselineDir)
     const cwd = process.cwd();
     afterEach(function () {
-        //本块代码会在每个测试用例执行之后执行
         process.chdir(cwd);
     });
-    for (let dir of dirs) {
+    for (const dir of dirs) {
         it(`emitter-simple-${dir}`, () => {
 
-
-            if (todos.indexOf(dir)) {
-                process.chdir(path.join(baselineDir, dir));
-                const content = fs.readFileSync('input.exml', 'utf-8');
-                typings.initTypings();
-                const skinNode = parser.generateAST(content)
-                fs.writeFileSync('1.log', JSON.stringify(skinNode, null, '\t'), 'utf-8');
-                const emitter = new JavaScriptEmitter();
-                const result = emitter.emit(skinNode);
-                fs.writeFileSync('2.log', result, 'utf-8');
-                const output = fs.readFileSync('expected-output.js', 'utf-8')
-                const resultAst = esprima.parseScript(result);
-                const outputAst = esprima.parseScript(output);
-                assert.deepEqual(resultAst, outputAst)
-
-            }
-            else {
-                // console.log(result)
-            }
-
-
+            process.chdir(path.join(baselineDir, dir));
+            const content = fs.readFileSync('input.exml', 'utf-8');
+            typings.initTypings();
+            const skinNode = parser.generateAST(content)
+            const emitter = new JavaScriptEmitter();
+            const result = emitter.emit(skinNode);
+            fs.writeFileSync('1.log', JSON.stringify(skinNode, null, '\t'), 'utf-8');
+            fs.writeFileSync('2.log', result, 'utf-8');
+            const output = fs.readFileSync('expected-output.js', 'utf-8')
+            const resultAst = esprima.parseScript(result);
+            const outputAst = esprima.parseScript(output);
+            assert.deepEqual(resultAst, outputAst)
         })
     }
 
