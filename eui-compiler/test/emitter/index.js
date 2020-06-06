66711496ea6480dadf7767f669a3df979c4af851
unknown <185713114@qq.com>
2020-06-06 14:19:04 +0800

    update testcase

--- eui-compiler/test/emitter/index.js
+++ eui-compiler/test/emitter/index.js
@@ -21,7 +21,7 @@ describe('javascript-emitter', () => {
         process.chdir(cwd);
     });
     for (const dir of dirs) {
-        it(`emitter-simple-${dir}`, () => {
+        it(`javascript-emitter-${dir}`, () => {
 
             process.chdir(path.join(baselineDir, dir));
             const content = fs.readFileSync('input.exml', 'utf-8');
@@ -29,13 +29,23 @@ describe('javascript-emitter', () => {
             const skinNode = parser.generateAST(content)
             const emitter = new JavaScriptEmitter();
             const result = emitter.generateJavaScriptAST(skinNode);
-            const text = codegen.generate(result);
             fs.writeFileSync('1.log', JSON.stringify(skinNode, null, '\t'), 'utf-8');
-            fs.writeFileSync('2.log', text, 'utf-8');
-            const output = fs.readFileSync('expected-output.js', 'utf-8')
-            const outputAst = esprima.parseScript(output);
-            assert.deepEqual(result, outputAst)
+            fs.writeFileSync('2.log', codegen.generate(result), 'utf-8');
+            const outputJavaScript = fs.readFileSync('expected-output-js.txt', 'utf-8')
+            const outputJavaScriptAst = esprima.parseScript(outputJavaScript);
+            assert.deepEqual(result, outputJavaScriptAst)
 
+        })
+
+        it('declaration-emitter', () => {
+            process.chdir(path.join(baselineDir, dir));
+            const content = fs.readFileSync('input.exml', 'utf-8');
+            const skinNode = parser.generateAST(content)
+            const emitter = new DeclarationEmitter();
+            emitter.emitSkinNode('input.exml', skinNode);
+            const result = emitter.getResult();
+            const outputDeclaration = fs.readFileSync("expected-output-d-ts.txt", 'utf-8');
+            assert.equal(outputDeclaration, result);
         })
     }
 
