75b01fc9588ef3b9fc3efeedb7f008ae0ba66070
unknown <185713114@qq.com>
2020-06-27 17:53:11 +0800

    add testcase

--- egret-webpack-bundler/test/index.js
+++ egret-webpack-bundler/test/index.js
@@ -1,2 +1,7 @@
-const lib = require('../lib/egretproject/index');
-lib.run('web', '../test-egret-project');
+//@ts-check
+const lib = require('../');
+const webpack = require('webpack');
+const path = require('path');
+const projectRoot = path.join(__dirname, 'simple-project');
+const bundler = new lib.EgretWebpackBundler(projectRoot, 'web');
+bundler.startDevServer({ libraryType: "debug" })
