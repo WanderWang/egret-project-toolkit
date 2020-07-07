b78d61bb7a9355538b57475a7848bbb7a03b7e92
unknown <185713114@qq.com>
2020-07-07 12:06:53 +0800

    update

--- packages/egret-library-installer/bin/run.js
+++ packages/egret-library-installer/bin/run.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
 const lib = require('../lib/index');
 const projectRoot = process.argv[3] || ".";
-console.log(projectRoot)
 lib.runCommand(projectRoot);
--- packages/egret-library-installer/src/runner.ts
+++ packages/egret-library-installer/src/runner.ts
@@ -8,6 +8,14 @@ type Egret_Property_JSON = {
 
 
 export async function runCommand(projectRoot: string) {
+    const comamand = process.argv[2];
+    const supportCommands = [
+        'build', 'publish'
+    ];
+    if (!supportCommands.includes(comamand)) {
+        console.log(`仅支持 ${supportCommands.join(",")} 命令`);
+        process.exit(1);
+    }
     const pkgFilePath = path.join(projectRoot, 'egretProperties.json');
     const json = await fs.readJsonAsync(pkgFilePath) as Egret_Property_JSON;
     const compileVersion = json.compilerVersion;
