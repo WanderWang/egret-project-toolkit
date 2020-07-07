f12da32c940324705cc435cc2388e81163b2686c
unknown <185713114@qq.com>
2020-07-07 11:08:44 +0800

    fix issue

--- packages/egret-library-installer/bin/cli.js
+++ packages/egret-library-installer/bin/cli.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 const lib = require('../lib/index');
-
-const lib = require('../lib/index');
 const engineVersion = process.argv[2];
 console.log('开始下载', engineVersion);
 lib.downloadAndInstall(engineVersion);
