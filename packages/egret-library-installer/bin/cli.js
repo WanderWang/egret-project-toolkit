af7cbb54e69d7f2a5fd16a688d6e9f386a4db1c4
unknown <185713114@qq.com>
2020-07-07 10:59:29 +0800

    add cli

---
+++ packages/egret-library-installer/bin/cli.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+const lib = require('../lib/index');
+
+const lib = require('../lib/index');
+const engineVersion = process.argv[2];
+console.log('开始下载', engineVersion);
+lib.downloadAndInstall(engineVersion);
