583dd841f0bea4073d458026bb85f1fd4da57129
unknown <185713114@qq.com>
2020-07-08 14:00:15 +0800

    重构代码

--- egret-webpack-bundler/src/egretproject/index.ts
+++ egret-webpack-bundler/src/egretproject/index.ts
@@ -1,8 +1,13 @@
 import { projectData, Target_Type } from "./data";
-import { manager } from "./manager";
 
 export function getLibsFileList(target: Target_Type, projectRoot: string, mode: "debug" | "release") {
     projectData.init(projectRoot)
-    const scripts = manager.copyLibsForPublish(target, mode);
-    return scripts;
-} 
+    const result: string[] = [];
+    projectData.getModulesConfig(target).forEach(m => {
+        m.target.forEach(m => {
+            const filename = mode == 'debug' ? m.debug : m.release;
+            result.push(filename);
+        });
+    });
+    return result;
+}
